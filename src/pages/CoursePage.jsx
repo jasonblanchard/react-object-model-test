80da4f0b819ad3366cf72e7c907aa58c8f9a1a5a
Jason Blanchard <jas.blanchard@gmail.com>
2016-07-31 09:47:24 -0400

    added eslint

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import React, { Component, PropTypes } from 'react';
 
 class CoursePage extends Component {
@@ -12,6 +13,7 @@ class CoursePage extends Component {
       <div>
         <h2>Course: {this.props.course.name}</h2>
         {this.props.course.topics.map(this.renderTopic)}
+        <Link to="courses">&lt;&lt; back to courses</Link>
       </div>
     );
   }
